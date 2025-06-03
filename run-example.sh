9d1f6de73561c99354bae8441deb5b3237dea5e5
tomer <101682824+tomernahum@users.noreply.github.com>
2025-06-03 16:08:46 -0400

    finish up?

--- run-example.sh
+++ run-example.sh
@@ -34,8 +34,13 @@
 # python src/main.py -v ./input_clips/105/105_warm_dirty_1_see3.mp4 -t ./input_timestamps/105/105_warm_dirty_see3_1.txt -o ./output_clips/105_see3
 # python src/main.py -v ./input_clips/105/105_warm_dirty_see3_2.mp4 -t ./input_timestamps/105/105_warm_dirty_see3_2.txt -o ./output_clips/105_see3
 
+# python src/main.py -v ./input_clips/106/106_clean_wet_see3.mp4 -t ./input_timestamps/106/106_clean_wet_see3.txt -o ./output_clips/106_see3
+
+# python src/main.py -v ./input_clips/106/106_warm_dirty_see3.mp4 -t ./input_timestamps/106/106_warm_dirty_see3.txt -o ./output_clips/106_see3
+
+
+# # --
+
+# python src/main.py -v ./input_clips/101/101_clean_wet_see3.mp4 -t ./input_timestamps/101/101_clean_wet_see3_ts.txt -o ./output_clips/101_see3
+# python src/main.py -v ./input_clips/103/103_warm_dirty_see3.mp4 -t ./input_timestamps/103/103_warm_dirty_see3.txt -o ./output_clips/103_see3
 python src/main.py -v ./input_clips/106/106_clean_wet_see3.mp4 -t ./input_timestamps/106/106_clean_wet_see3.txt -o ./output_clips/106_see3
-
-python src/main.py -v ./input_clips/106/106_warm_dirty_see3.mp4 -t ./input_timestamps/106/106_warm_dirty_see3.txt -o ./output_clips/106_see3
-
-
